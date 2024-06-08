01d3c8c39960c98fe78330225d3d7d05ac803f35
Adrian Jimenez <135673699+Keyroguepuma@users.noreply.github.com>
2024-06-07 23:19:42 -0700

    update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-
+const port = 3000;
 app.get('/',(req,res) => {
     res.send("Hello World")
 })
 
 
-app.listen(3000,console.log("listening in port 3000"));
+app.listen(3000,console.log(`Listening in port ${port}`));
