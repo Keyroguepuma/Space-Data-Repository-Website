bc059660517dd328c015f03475bdd9fe2e6c5322
Adrian Jimenez <ajimenez2266@gmail.com>
2024-06-08 01:04:03 -0700

    Got HBS to work

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+
 const app = express();
 const port = 3000;
+
+app.set('view engine', 'hbs');
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
 app.get('/',(req,res) => {
-    res.send("Hello World")
+    res.render('index' , {title: 'Home'}, console.log("It worded"));
 })
 
 
