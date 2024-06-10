b69c44b68eae791c4368249d918641efb6a66543
Adrian Jimenez <ajimenez2266@gmail.com>
2024-06-10 16:29:07 -0700

    registration and database

--- BackEnd/routes/users.js
+++ BackEnd/routes/users.js
@@ -35,4 +35,31 @@ router.get('/registration', (req,res) => {
     res.render('account',{title: 'Sign In', webTitle: 'Space Data Repository'})
 })
 
+router.post('/registration', async function(req,res,next){
+    var{firstName,lastName,username,email,password,cpassword,role} = req.body;
+    var emailConfirmation = 0;
+
+    if(role){
+        emailConfirmation = 1;
+    }
+
+    try {
+        const insertAccountQuery = 'INSERT INTO `Account`(accountName,email,password,firstName,lastName,emailUpdate) Values(?,?,?,?,?,?)';
+        const [results,fields] = await db.query(insertAccountQuery,[username,email,password,firstName,lastName,emailConfirmation]);
+
+        if(results && results.affectedRows == 1){
+            res.redirect('./login')
+        }else{
+            res.redirect('./registration');
+        }
+
+    } catch (error) {
+        next(error);
+        
+    }
+
+})
+
+
+
 module.exports = router;
